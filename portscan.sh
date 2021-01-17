0865256ea2bc33bedebfac77779705ac4f73fe73
jam620 <jam620@gmail.com>
2021-01-16 21:12:31 -0500

    Final

---
+++ portscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# ./portscan.sh <ip-address>
+
+if [ $1 ]; then
+	ip_address=$1
+	for port in $(seq 1 65555); do
+		timeout 1 bash -c "echo '' > /dev/tcp/$ip_address/$port" 2>/dev/null && echo "[*] Port $port - OPEN" &
+	done; wait
+
+else
+	echo -e "\n[*] Uso: ./portscan.sh <ip-address>\n"
+	exit 1
+fi
